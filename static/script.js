daee8cf837733bf5c0d25e198bf6316e1af12b92
TheGenocide <82718066+TheGenocides@users.noreply.github.com>
2024-05-05 01:01:31 +0000

    hello world

---
+++ static/script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburgers").addEventListener("click", (ev) => {
+    document.getElementById("navitems").classList.toggle("trigger-navitems")
+    document.getElementById("hamburgers").classList.toggle("change")
+})
